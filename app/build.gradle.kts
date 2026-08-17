56d0a99969a94ec4ee1611cbb32aafeb88a15cb8
ums1212 <voll1212@gmail.com>
2026-08-17 22:16:44 +0900

    OAuth 2.0 클라이언트 ID 설정 수정
    release,debug 분리를 default로 통일

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.comon.toss_watch"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
