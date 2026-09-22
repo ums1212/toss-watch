4303aea58455b9b748053f86b0d9530b6816da91
ums1212 <voll1212@gmail.com>
2026-09-22 23:45:17 +0900

    chore: app, watch-app version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.comon.toss_watch"
         minSdk = 26
         targetSdk = 37
-        versionCode = 8
-        versionName = "0.0.8"
+        versionCode = 9
+        versionName = "0.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
